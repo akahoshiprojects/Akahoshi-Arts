f3999953c6559a264ce74c2226ee7aa256ac6b4a
akahoshiprojects <akahoshiprojects@gmail.com>
2026-07-06 19:49:50 -0300

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,8 +1,36 @@
 const aiArts = [
 
     {
-        image: "AI01.png",
-        tags: ["Jujutsu Kaisen", "Gender Bender"]
+        image: "Ai01.png",
+        name: "Ryomen Sukuna",
+        tags: [
+            "Jujutsu Kaisen",
+            "Gender Bender"
+        ]
     }
 
 ];
+
+
+const gallery = document.querySelector(".gallery-grid");
+
+
+aiArts.forEach(art => {
+
+    const item = document.createElement("div");
+
+    item.classList.add("card-item");
+
+
+    item.innerHTML = `
+
+        <img
+            src="Assets/AI/${art.image}"
+            alt="${art.name}">
+
+    `;
+
+
+    gallery.appendChild(item);
+
+});
