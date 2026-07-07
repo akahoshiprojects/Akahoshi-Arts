857de2740a8e3e3f5fb6f8d07762aefb7863530b
akahoshiprojects <akahoshiprojects@gmail.com>
2026-07-07 13:46:06 -0300

    Update ai.js

--- ai.js
+++ ai.js
@@ -65,13 +65,19 @@ function loadGallery(list) {
 
             <div class="ai-info">
 
-                <span class="ai-name">${art.name}</span>
+                <span class="ai-name">
+        ${art.name}
+    </span>
 
-                ${art.tags.map(tag =>
-                    `<span class="tag">${tag}</span>`
-                ).join("")}
+    ${art.series ? 
+        `<span class="ai-series">${art.series}</span>` 
+        : ""}
 
-            </div>
+    ${art.tags.map(tag =>
+        `<span class="tag">${tag}</span>`
+    ).join("")}
+
+</div>
 
         `;
 
