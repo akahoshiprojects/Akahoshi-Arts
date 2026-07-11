36c53bf73eb06ef8fee55e9305b2157ac603cf41
akahoshiprojects <akahoshiprojects@gmail.com>
2026-07-10 22:27:52 -0300

    Create viewer.js

---
+++ viewer.js
@@ -0,0 +1,43 @@
+const viewer = document.querySelector(".image-viewer");
+const viewerImage = viewer.querySelector("img");
+const closeViewer = document.querySelector(".viewer-close");
+
+
+document.querySelectorAll("img").forEach(img => {
+
+    if (
+        img.closest(".home-button") ||
+        img.closest(".request-button") ||
+        img.closest(".gallery-title")
+    ) {
+        return;
+    }
+
+
+    img.addEventListener("click", () => {
+
+        viewerImage.src = img.src;
+
+        viewer.style.display = "flex";
+
+    });
+
+});
+
+
+closeViewer.addEventListener("click", () => {
+
+    viewer.style.display = "none";
+
+});
+
+
+viewer.addEventListener("click", (e) => {
+
+    if (e.target === viewer) {
+
+        viewer.style.display = "none";
+
+    }
+
+});
